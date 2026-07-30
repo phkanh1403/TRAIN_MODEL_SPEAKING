e841129248369c19b4e399ba62399f84549e607f
phkimanh <phamhoangkimanh3@gmail.com>
2026-07-30 15:00:43 +0700

    thêm phần sửa stt và thêm dữ liệu

--- STREAMLIT_SPEAKING.py
+++ STREAMLIT_SPEAKING.py
@@ -36,26 +36,30 @@ with tab2:
     st.header("SỬA DỮ LIỆU")
     col4,col5,col6 = st.columns(3)
     with col4:
-        id_fix = st.number_input("Nhập dòng sửa")
+        id_fix_input = st.number_input("Nhập dòng sửa")
     with col5:
         id_cluster_fix = st.number_input("Nhập stt sửa")
     with col6:
         part_fix = st.number_input("Nhập phần sửa")
-    id_fix = str(int(id_fix))+"."+str(int(id_cluster_fix))+"."+str(int(part_fix))
+    id_fix = str(int(id_fix_input))+"."+str(int(id_cluster_fix))+"."+str(int(part_fix))
 
     st.subheader("NHẬP DỮ LIỆU CẦN SỬA")
     part_fix_new = st.number_input("Nhập phần sửa lại")
+    id_cluster_fix_new = st.number_input("Nhập stt sửa lại")
     question_fix = st.text_area("Nhập câu hỏi sửa")
     answer_fix = st.text_area("Nhập câu trả lời sửa")
     context_fix = st.text_input("Nhập chủ đề sửa")
     score_fix = st.number_input("Nhập điểm sửa")
     if st.button("Sửa"):
+        id_fix_new = str(int(id_fix_input))+"."+str(int(id_cluster_fix_new))+"."+str(int(part_fix_new))
         mask = df["ID"] == id_fix
+        df.loc[mask, "ID"] = id_fix_new
         df.loc[mask, "QUESTION"] = question_fix
         df.loc[mask, "ANSWER"] = answer_fix
         df.loc[mask, "CONTEXT"] = context_fix
         df.loc[mask, "SCORE"] = score_fix
         df.loc[mask, "CONTEXT_CLUSTER"] = part_fix_new
+        df.loc[mask, "ID_CLUSTER"] = id_cluster_fix_new
         df.to_csv(
             r"D:\TRAIN_MODEL_SPEAKING\SPEAKING_DATASET.csv",index=False,encoding="utf-8-sig")
         st.write("Đã sửa")
